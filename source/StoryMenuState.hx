f80a34b9c8349348150981a3777d4712865ed90e
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-01-29 08:57:20 +0200

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -181,6 +181,8 @@ class StoryMenuState extends MusicBeatState
 
 		changeWeek();
 
+                addVirtualPad(FULL, A_B_X_Y);
+
 		super.create();
 	}
 
@@ -235,12 +237,12 @@ class StoryMenuState extends MusicBeatState
 			else if (upP || downP)
 				changeDifficulty();
 
-			if(FlxG.keys.justPressed.CONTROL)
+			if(FlxG.keys.justPressed.CONTROL#if android || _virtualpad.buttonX.justPressed #end)
 			{
 				persistentUpdate = false;
 				openSubState(new GameplayChangersSubstate());
 			}
-			else if(controls.RESET)
+			else if(controls.RESET#if android || _virtualpad.buttonY.justPressed #end)
 			{
 				persistentUpdate = false;
 				openSubState(new ResetScoreSubState('', curDifficulty, '', curWeek));
